49889a7f90921ce68ebfab41c8b187d491c92814
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2021-04-01 16:55:43 +0200

    1️⃣  V.1.19 🎉 Added Success component to Completed step on F1SC ✅ | Ready for production? 🎉

--- client/src/components/RegisterTenancy(F1)/completed.jsx
+++ client/src/components/RegisterTenancy(F1)/completed.jsx
@@ -2,20 +2,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// Imported Styles
-import styles from "./register-user.module.scss";
+// Custom Components
+import Success from "../Success/Success";
 
 // Multi language
 import { withNamespaces } from "react-i18next";
 
 const Completed = ({ tenancy, t }) => {
   return (
-    <div className={styles.CompleteContainer}>
-      <div className={styles.CompleteText}>
-        <h1>{t("F1SC.completed.title")}</h1>
-        <h3>{t("F1SC.completed.subtitle")} </h3>
-      </div>
-    </div>
+    <>
+      <Success
+        title={t("F1SC.completed.title")}
+        subtitle={t("F1SC.completed.subtitle")}
+      />
+    </>
   );
 };
 
